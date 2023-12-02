0e84d4500be87afe4efb0c43faad201e097d44c3
panwar2001 <ayushpanwar691@gmail.com>
2023-12-02 18:29:31 +0530

    add server

--- server/index.js
+++ server/index.js
@@ -6,14 +6,8 @@ app.use(cors());
 const fs=require('fs')
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const bcrypt = require("bcrypt");
-const mongoose = require("mongoose");
 const multer= require('multer')
 const axios=require('axios')
-mongoose.set("strictQuery", true);
-
-// const User = require("User");
-const uri = process.env.URI;
 const storage = multer.memoryStorage(); 
 const ocr=require('./ocr');
 const upload = multer({
@@ -30,50 +24,6 @@ const upload = multer({
     fileSize:7340032  // 1024 X 1024 X 7 MB
   },
 });
-
-
-// mongoose
-//   .connect(uri)
-//   .then(() => console.log("Connected to MongoDB Atlas!"))
-//   .catch((err) => console.error("Error connecting to MongoDB Atlas:", err));
-
-// app.post("/signup", async (req, res) => {
-//   const { name, email, password } = req.body;
-//   const data = await User.findOne({ email: email });
-//   if (data) {
-//     res.send({ status: 3 });
-//   } else {
-//     const salt = bcrypt.genSaltSync(10);
-//     const hashedPassword = bcrypt.hashSync(password, salt);
-//     try {
-//       const temp = new User({
-//         name: name,
-//         email: email,
-//         password: hashedPassword,
-//       });
-//       const data = await temp.save();
-//     } catch (error) {
-//       console.log(error);
-//     }
-//     res.send({ status: 1 });
-//   }
-// });
-
-// app.post("/login", async (req, res) => {
-//   const { email, password } = req.body;
-//   const data = await User.findOne({ email: email });
-//   if (data) {
-//     const temp = bcrypt.compareSync(password, data.password);
-//     if (temp) {
-//       console.log(temp);
-//       res.send({ status: 2 });
-//     } else {
-//       res.send({ status: -1 });
-//     }
-//   } else {
-//     res.send({ status: 0 });
-//   }
-// });
 
 app.post('/upload',async (req,res)=>{
   upload.single('file')(req, res, (err) => {
