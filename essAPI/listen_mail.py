7253870e5ace588dbc0fe1e6e93fc3dd306b8d8b
negia7265 <negia7265@gmail.com>
2024-01-03 11:03:18 +0530

    added latest pdf fetching route

--- essAPI/listen_mail.py
+++ essAPI/listen_mail.py
@@ -1,22 +1,16 @@
-from flask import Flask, request, jsonify, render_template, send_file
-# flask extension for handling cross origin resource sharing(CORS), making cross origin possible.
-from flask_cors import CORS
-from invoiceAI import InvoiceParser
-from io import BytesIO
+from flask import Flask, send_file
 import imaplib
 import email
 from email.header import decode_header
 import os
 import io
-app = Flask(__name__, template_folder='template')
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
-# configuring cors headers to content type
-app.config['CORS_HEADERS'] = 'Content-Type'
-
-# Function to return the latest pdf attachment from the email inbox in bytes format
+app = Flask(__name__)
 
 
 def fetch_latest_pdf_attachment(email_user, email_pass):
+    # Connect to the IMAP server
+    # email_user = 'blackpearl7579@gmail.com'
+    # email_pass = 'ddvh wnep mxmr ydso'
 
     # Connect to the IMAP server
     mail = imaplib.IMAP4_SSL("imap.gmail.com")
@@ -34,52 +28,40 @@ def fetch_latest_pdf_attachment(email_user, email_pass):
     _, latest_msg_data = mail.fetch(latest_msg_id, "(RFC822)")
     latest_raw_email = latest_msg_data[0][1]
     latest_msg = email.message_from_bytes(latest_raw_email)
+    print(type(latest_msg))
+    # Check if the email has PDF attachments
+    # latest_pdf_attachment = None
+    # for part in latest_msg.iter_attachments():
+    #     if part.get_content_type() == "application/pdf":
+    #         latest_pdf_attachment = part.get_payload(decode=True)
+    #         break
     pdf_attachment = None
     for part in latest_msg.walk():
         if part.get_content_type() == "application/pdf":
             print("PDF found")
             pdf_attachment = part.get_payload(decode=True)
             break
+    print(type(pdf_attachment))
     # Process the PDF attachment
     if pdf_attachment:
         # Save the PDF content to a file with the appropriate extension
         pdf_filename = "latest_attachment.pdf"
         with open(pdf_filename, "wb") as pdf_file:
             pdf_file.write(pdf_attachment)
+    # # Process the latest PDF attachment
+    # if latest_pdf_attachment:
+    #     # Extract text from PDF using pdfminer
+    #     # pdf_text = extract_text(latest_pdf_attachment)
+
+    #     # You can now process the extracted text as needed
+    #     print(f"Extracted text from the latest PDF in email")
+    # else:
+    #     print("No PDF attachment found in the latest email.")
 
     # Close the connection
     mail.close()
     mail.logout()
     return pdf_attachment
-
-
-@app.route('/')
-def index():
-    return render_template('404.html'), 404
-
-
-@app.route('/parse_invoice/api', methods=['POST'])
-def extract_invoice_pdf():
-    if len(request.files) != 1:
-        response = jsonify(
-            {'error': 'Invalid number of invoice ! please upload 1 invoice image/pdf to process.'})
-        # TODO check
-        response.status_code = 404
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        return response
-    FILE = request.files['file']
-    if FILE.mimetype == 'application/pdf':
-        parser = InvoiceParser(BytesIO(FILE.read()), 'pdf')
-        response = jsonify(parser.getData())
-    elif FILE.mimetype.startswith('image'):
-        parser = InvoiceParser(FILE.read(), 'image')
-        response = jsonify(parser.getData())
-    else:
-        # TODO check
-        response = jsonify({'error': 'Invalid file type.'})
-        response.status_code = 404
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response  # return json data to client side
 
 
 @app.route('/get_latest_pdf', methods=['GET'])
@@ -104,4 +86,4 @@ def get_latest_pdf():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Please do not set debug=True in production
+    app.run(debug=True)
