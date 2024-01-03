8c72c10643eb1ae7b2672ffb735f34c6b089d08b
negia7265 <negia7265@gmail.com>
2024-01-03 19:32:15 +0530

    fixed location.json error

--- essClient/src/Navbar.jsx
+++ essClient/src/Navbar.jsx
@@ -81,16 +81,16 @@ const Navbar = () => {
               marginRight: "10vh",
             }}
           >
-            <Logo
+            {/* <Logo
               style={{ cursor: "pointer" }}
               src="https://firebasestorage.googleapis.com/v0/b/ess-authentication-fb04c.appspot.com/o/google-maps-svgrepo-com.svg?alt=media&token=70b1ba0f-e8a8-40d9-b47e-b9b52e5346f7"
-            ></Logo>
-            {/* <StyledList href="/">
+            ></Logo> */}
+            <StyledList href="/">
               <div>
                 <i style={{ fontSize: "8vh" }} class="fa-solid fa-upload"></i>
                 <h4>Upload</h4>
               </div>
-            </StyledList> */}
+            </StyledList>
           </div>
         </div>
       </ContainerNav>
